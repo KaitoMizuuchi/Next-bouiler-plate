2b945bc48a6b1649b969c5f5d4aa2fa909529577
KaitoMizuuchi <kaito.mizuchi@g.dreamcareer.co.jp>
2024-12-13 18:37:13 +0900

    フォルダ構成実装

---
+++ src/app/about/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    // TODO: ページ作成
+    // HACK: 一時的な解決策。修正が必要であることを示唆。コードが長くてリファクタ必要か
+    // FIXME: 修正をする必要があるところ
+    // XXX: ほかの人に注意を促す
+    // BUG: これはバグですかね
+    // [ ]: 未完了のタスク
+    // [x]: 完了済み
+    <div>About</div>
+  );
+};
+
+export default About;
