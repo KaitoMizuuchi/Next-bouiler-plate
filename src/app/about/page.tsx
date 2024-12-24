fe8ba7f815b03b4f0caa1e9041d2bb84d12a83ef
KaitoMizuuchi <kaito.mizuchi@g.dreamcareer.co.jp>
2024-12-24 16:23:47 +0900

    prettierとeslintの設定

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from 'react';
 
 const About = () => {
+  const [todo, setTodo] = useState();
   return (
     // TODO: ページ作成
     // HACK: 一時的な解決策。修正が必要であることを示唆。コードが長くてリファクタ必要か
@@ -9,7 +10,12 @@ const About = () => {
     // BUG: これはバグですかね
     // [ ]: 未完了のタスク
     // [x]: 完了済み
-    <div>About</div>
+    // Tot
+
+    <>
+      <div>About</div>
+      <h1>こんにちは</h1>
+    </>
   );
 };
 
