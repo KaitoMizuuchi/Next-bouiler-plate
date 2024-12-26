cc9224abaa1c3a754f35b66fc2f56b07c24b2b9c
KaitoMizuuchi <kaito.mizuchi@g.dreamcareer.co.jp>
2024-12-26 15:21:03 +0900

    prettierのimport空白削除

--- src/app/calc/page.tsx
+++ src/app/calc/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import styles from './calc.module.scss';
 
 const page = () => {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-
 import '@/styles/global/global.scss';
-
 import Header from '@/components/layout/header/Header';
 
 const geistSans = Geist({
